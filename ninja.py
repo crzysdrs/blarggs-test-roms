7ebed287c03c83758d3e209958c0108395ad2508
Mitch Souders <mitch.souders@gmail.com>
2019-07-29 11:42:48 -0700

    CPU Instrs Match

---
+++ ninja.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import ninja_syntax
+import glob
+import re
+import os.path
+
+dirs = [
+    #"cgb_sound",
+    "cpu_instrs",
+    # "dmg_sound",
+#     "instr_timing",
+#     "interrupt_timing",
+#     "mem_timing",
+#     "mem_timing-2",
+#     "oam_bug",
+#
+]
+
+n = ninja_syntax.Writer(open("build.ninja", "w"))
+
+n.rule("compile", "wla-gb -I common $dmg_or_cgb -D TEST_NAME=\"$test_name\" $in")
+n.rule("makelink", "./makelink $out $in")
+n.rule("link", "wlalink -d \"$linkfile\" $out")
+n.rule("checksums", "cd roms && md5sum --ignore-missing -c ../$sumfile")
+
+roms = []
+for d in dirs:
+    for s in glob.glob("{}/*.s".format(d)):
+        (name, ext) = os.path.splitext(s)
+        print (name)
+        compile_vars = {"test_name": os.path.basename(name)}
+        if d == "dmg_sound":
+            compile_vars['dmg_or_cgb'] = "-D REQUIRE_DMG"
+        elif d == "cgb_sound":
+            compile_vars['dmg_or_cgb'] = "-D REQUIRE_CGB"
+        objs = n.build("{}.o".format(name), rule="compile", inputs=[s], variables=compile_vars)
+        linkfile = n.build("{}.link".format(name), rule="makelink", inputs=objs)
+        link_vars={"linkfile": linkfile[0]}
+        roms.extend(n.build("roms/{}.gb".format(name), rule="link", inputs=linkfile + objs, variables=link_vars))
+
+n.build("pass", rule="checksums", inputs=roms, variables={"sumfile":"md5sums"})
